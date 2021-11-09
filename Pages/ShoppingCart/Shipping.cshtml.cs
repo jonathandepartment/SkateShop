430d1ef11a0dca23302e7e4ee68eb2c354ab60ea
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2021-11-09 15:56:40 +0100

    Created form for Shipping

--- Pages/ShoppingCart/Shipping.cshtml.cs
+++ Pages/ShoppingCart/Shipping.cshtml.cs
@@ -9,6 +9,16 @@ namespace SkateShop.Pages.ShoppingCart
 {
     public class ShippingModel : PageModel
     {
+        [BindProperty]
+        public string FirstName { get; set; }
+        [BindProperty]
+        public string LastName { get; set; }
+        [BindProperty]
+        public string Address { get; set; }
+        [BindProperty]
+        public string PhoneNumber { get; set; }
+        [BindProperty]
+        public double FreightCost { get; set; }
         public void OnGet()
         {
 
