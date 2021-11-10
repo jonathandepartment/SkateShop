f0905f4052a90655849ef89b96138bcee496ce5d
Moustafa <moustafaallouhaibi@gmail.com>
2021-11-10 11:03:08 +0100

    Linking Model to Razor Page

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SkateShop.Models;
 
 namespace SkateShop.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        public List<ProductModel> Products = new List<ProductModel>();
+
         public void OnGet()
         {
+            this.Products = Data.ProductManager.GetProducts();
+          
+
         }
     }
 }
