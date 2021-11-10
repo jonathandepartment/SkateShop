fcdb0d11ae1786d11459a6fea9f5fbfdff2cd31e
Moustafa <moustafaallouhaibi@gmail.com>
2021-11-10 22:58:28 +0100

    Started with Admins Edit functionality

---
+++ Pages/Admin/Edit/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkateShop.Pages.Admin.Edit
+{
+    public class IndexModel : PageModel
+    {
+        public Models.ProductModel Product { get; set; }
+        public void OnGet()
+        {
+            //Data.ProductManager.GetProducts();
+            //Product = Data.ProductManager.GetProduct(id);
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
